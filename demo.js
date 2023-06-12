edab38d3449d8f1bf1935659580c157bd967cb87
gsanchez21 <medina21g@yahoo.com>
2023-06-12 10:00:02 -0400

    First Commit

---
+++ demo.js
@@ -0,0 +1,9 @@
+console.log('before timeout')
+setTimeout(() => {
+    console.log('inside timeout')
+}, 3000);
+console.log('after timeout');
+
+//setTimeout(5000);
+console.log('after after timeout'); 
+ 
